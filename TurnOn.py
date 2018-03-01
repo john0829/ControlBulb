3871f9780848af3132593a68665a4833f7a7e8ee
john0829 <joh860829nny@gmail.com>
2018-03-01 12:04:29 +0800

    add warning false

--- TurnOn.py
+++ TurnOn.py
@@ -2,9 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 LED_PIN = 3
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 GPIO.output(LED_PIN, 1)
-GPIO.setwarnings(False)
+
 # GPIO.cleanup()
